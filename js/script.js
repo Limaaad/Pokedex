573e91c8096b6562dad7d6ab5b32f8f9be83bf3d
Limaaad <fabielledelima@gmail.com>
2024-02-10 13:25:24 -0300

    adicionando imagens javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const pokemonName = document.querySelector('.poke-name');
+const pokemonNumber = document.querySelector('.poke-number');
+const pokemonImage = document.querySelector('.poke-img');
+
+
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await APIResponse.json();
@@ -6,6 +11,10 @@ const fetchPokemon = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
+    ['animated']['front_default'];
 }
 
-renderPokemon('charizard')
+renderPokemon('6')
